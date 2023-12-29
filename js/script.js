d0c5c8d9242c81bdddb56efab5a7bba96413f072
kervines <kervinzero@gmail.com>
2023-12-29 00:43:44 -0300

    fix - corrigido bug que nao escrevia todos os textos da array

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 const autoText = document.querySelector('[data-text]');
-const texts = 'cara legal';
-let i = 0;
+const texts = ['a Programmer', 'a Developer FrontEnd', '@kervines.dev'];
+let charIndex = 0;
+let textArrayIndex = 0;
 
-const auto = setInterval(setText, 200);
+setInterval(setText, 400);
 function setText() {
-  autoText.innerText += texts[i].split('');
-  i++;
-  if (i === texts.length) {
-    clearInterval(auto);
+  autoText.innerText += texts[textArrayIndex].charAt(charIndex);
+  charIndex++;
+  if (charIndex === texts[textArrayIndex].length + 1) {
+    autoText.innerText = '';
+    charIndex = 0;
+    textArrayIndex++;
+  }
+  if (textArrayIndex === texts.length) {
+    textArrayIndex = 0;
   }
 }
